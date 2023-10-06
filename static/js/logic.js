94600bf8c921012225f60f2c55226d8230ef74fa
Robert Pearce <robertgraftonpearce@gmail.com>
2023-10-05 22:05:51 -0400

    modifications calling json via html in flask

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,1 +1,8 @@
-// This will be for our java script for the mapping and DOM manipulation
+const url = "http://127.0.0.1:5000/api/v1.0/weatherdata"
+
+let weatherJson = d3.json(url).then(run);
+
+function run(dataset) {
+   //console.log(dataset)
+};
+console.log(weatherJson);
